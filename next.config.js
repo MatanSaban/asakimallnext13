43553c403b0aec7c580d01e5d3f24f426978b394
MatanSaban <105128031+MatanSaban@users.noreply.github.com>
2023-02-21 09:46:11 +0200

    123

--- next.config.js
+++ next.config.js
@@ -10,6 +10,8 @@ const nextConfig = {
     env: {
       DATABASE_URL:
         "mongodb+srv://asakimalladmin:VNbEXSR44SDrPMO2@asakimall1.ss5v6hb.mongodb.net/Asakimall?retryWrites=true&w=majority",
+      MONGODB_URI:
+        "mongodb+srv://asakimalladmin:VNbEXSR44SDrPMO2@asakimall1.ss5v6hb.mongodb.net/Asakimall?retryWrites=true&w=majority",
     },
     images: {
       domains: ["greendiamonds.co.il"],
